ba05dced94037cf0dc7304ce556af442b0f21278
DenisHrytsai242 <ipz242_gdv@student.ztu.edu.ua>
2026-04-23 22:38:05 +0300

    added gitignore

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ function formulaCalculating(rate, usableNumber = 1, one = "1") {
     if (sum !== null) return sum;
     return null;
 }
-console.log(formulaCalculating(42
-));
+console.log(formulaCalculating(42));
 // (((1 * 3) + 5) + 5)
+//I have done it using AI, I have no idea how it works overall
+// я прочитал много и искал how to, но так и не понял.
+// С факториалом я понял и очень легко сделал, а здесь так и не смог
