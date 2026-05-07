953341b3d7b6c8d4a89b5cb304d4cef200275e79
DenisHrytsai242 <ipz242_gdv@student.ztu.edu.ua>
2026-05-07 15:18:14 +0300

    redone to coreect

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-function formulaCalculating(rate, usableNumber = 1, one = "1") {
-    if (usableNumber === rate) {
-        return one;
+const formulaCalculating = (point) =>{
+    function func(first, second){
+        if (first === point){
+            return second;
+        }
+        if (first > point){
+            return null;
+        }
+        return func(first* 3, `(${second} * 3)`) || func( first + 5 , `(${second} + 5 )`)
     }
-    if (usableNumber > rate) {
-        return null;
-    }
-    let multiply = formulaCalculating(rate, usableNumber * 3, `(${one} * 3)`);
-    if (multiply !== null) return multiply;
-    let sum = formulaCalculating(rate, usableNumber + 5, `(${one} + 5)`);
-    if (sum !== null) return sum;
-    return null;
+    return func(1, "1")
 }
-console.log(formulaCalculating(42));
-// (((1 * 3) + 5) + 5)
-//I have done it using AI, I have no idea how it works overall
-// я прочитал много и искал how to, но так и не понял.
-// С факториалом я понял и очень легко сделал, а здесь так и не смог
+console.log(formulaCalculating(27));
